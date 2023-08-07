4ab41a9f4424f3686c11e10e3b4f22e20bdab2ac
mlc-kawabata-shohei <kawabata-shohei@mitsue.co.jp>
2023-08-07 15:15:52 +0900

    brashup

--- components/post-header.js
+++ components/post-header.js
@@ -6,7 +6,7 @@ import PostTitle from '../components/post-title'
 export default function PostHeader({ title, coverImage, date, author }) {
   return (
     <>
-      <PostTitle>{title}</PostTitle>
+      <PostTitle>{title}!!!</PostTitle>
       <div className="hidden md:block md:mb-12">
         {author && <Avatar name={author.name} picture={author.picture} />}
       </div>
